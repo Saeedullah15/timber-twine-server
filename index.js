410b54c6ece58c7d298e721faadf1389d2c1cdb7
Saeedullah15 <saeedullahazim65@gmail.com>
2024-08-19 18:39:41 +0600

    git ignore issue solved fully and env variable added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,8 +9,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-
-const uri = "mongodb+srv://ArtAndCraftStore:b0boYGlntrnJ3bKW@cluster0.yyrxfdz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// connection string
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yyrxfdz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
