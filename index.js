7ec305c81abe747ed56a6c53c18daa8a13d18a83
Saeedullah15 <saeedullahazim65@gmail.com>
2024-08-20 19:32:15 +0600

    delete api implemented

--- index.js
+++ index.js
@@ -76,6 +76,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete("/deleteCraftItem/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await craftCollection.deleteOne(query);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
